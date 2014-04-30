0b20afe331b98ec0ad36975e848d24ba6d129a67
Sam Stavinoha <smlstvnh@gmail.com>
2014-04-30 14:06:21 -0500

    use satori and make this a module

--- forward.py
+++ forward.py
@@ -6,21 +6,15 @@ try:
 except ImportError:
     import socketserver as SocketServer
 
-import sys
-from optparse import OptionParser
+from satori import ssh
 
-import paramiko
-
-SSH_PORT = 22
-DEFAULT_PORT = 4000
-
-g_verbose = True
+DEFAULT_REMOTE_PORT = 4000
 
 
 class ForwardServer (SocketServer.ThreadingTCPServer):
     daemon_threads = True
     allow_reuse_address = True
-    
+
 
 class Handler (SocketServer.BaseRequestHandler):
 
@@ -30,17 +24,15 @@ class Handler (SocketServer.BaseRequestHandler):
                                                    (self.chain_host, self.chain_port),
                                                    self.request.getpeername())
         except Exception as e:
-            verbose('Incoming request to %s:%d failed: %s' % (self.chain_host,
+            print ('Incoming request to %s:%d failed: %s' % (self.chain_host,
                                                               self.chain_port,
                                                               repr(e)))
             return
         if chan is None:
-            verbose('Incoming request to %s:%d was rejected by the SSH server.' %
-                    (self.chain_host, self.chain_port))
+            print ('Incoming request to %s:%d was rejected by the SSH server.' %
+                   (self.chain_host, self.chain_port))
             return
 
-        verbose('Connected!  Tunnel open %r -> %r -> %r' % (self.request.getpeername(),
-                                                            chan.getpeername(), (self.chain_host, self.chain_port)))
         while True:
             r, w, x = select.select([self.request, chan], [], [])
             if self.request in r:
@@ -53,27 +45,69 @@ class Handler (SocketServer.BaseRequestHandler):
                 if len(data) == 0:
                     break
                 self.request.send(data)
-                
+
         peername = self.request.getpeername()
         chan.close()
         self.request.close()
-        verbose('Tunnel closed from %r' % (peername,))
+        print 'Tunnel closed from %r' % peername
 
 
-def forward_tunnel(local_port, remote_host, remote_port, transport):
-    # this is a little convoluted, but lets me configure things for the Handler
-    # object.  (SocketServer doesn't give Handlers any way to access the outer
-    # server normally.)
-    class SubHander (Handler):
-        chain_host = remote_host
-        chain_port = remote_port
-        ssh_transport = transport
-    ForwardServer(('', local_port), SubHander).serve_forever()
+class Tunnel(object):
 
+    def __init__(self, remote_host, bastionclient,
+                 remote_port=DEFAULT_REMOTE_PORT, local_port=None):
 
-def verbose(s):
-    if g_verbose:
-        print(s)
+        # Nico,
+        # perhaps this class (or this module) should be the one who handles which
+        # local_port is bound to this tunnel (hence local_port=None),
+        # that way the end instantiating the
+        # class instance can look to see where its running...
+        # this would assume that the user of this class doesnt actually care
+        # which port the Tunnel ends up on and that this class would handle any
+        # attempts to bind to a port that's already being used
+
+        self.remote_host = remote_host
+        self.remote_port = remote_port
+        self.bastionclient = bastionclient
+        if not local_port:
+            self.local_port = self.get_available_localport()
+        else:
+            # check if local_port is available else raise exception
+            pass
+        self._tunnel = None
+
+    def serve(self):
+        self._ssh_transport = self.bastionclient.get_transport()
+
+        # this is gross
+
+        #############################################
+        # this is a little convoluted, but lets me configure things for the Handler
+        # object.  (SocketServer doesn't give Handlers any way to access the outer
+        # server normally.)
+        #############################################
+
+        class SubHandler(Handler):
+            chain_host = self.remote_host
+            chain_port = self.remote_port
+            ssh_transport = self._ssh_transport
+
+        self._tunnel = ForwardServer(('', self.local_port), SubHandler)
+        # TODO:
+        # implement async that Nico is working on for this call
+        self._tunnel.serve_forever()
+
+    def get_available_localport(self):
+        """Find an available ephemeral port and return it."""
+        found = None
+        while not found:
+            # todo lookup ephemeral ports range
+            temp = random.randint(40000, 70000)
+            # ping temp, "reserve it" and set `found` if available
+            # if temp.ok:
+            found = temp
+
+        return found
 
 
 HELP = """\
@@ -83,79 +117,22 @@ the SSH server. This is similar to the openssh -L option.
 """
 
 
-def get_host_port(spec, default_port):
-    "parse 'hostname:22' into a host and port, with the port optional"
-    args = (spec.split(':', 1) + [default_port])[:2]
-    args[1] = int(args[1])
-    return args[0], args[1]
-
-
-def parse_options():
-    global g_verbose
-    
-    parser = OptionParser(usage='usage: %prog [options] <ssh-server>[:<server-port>]',
-                          version='%prog 1.0', description=HELP)
-    parser.add_option('-q', '--quiet', action='store_false', dest='verbose', default=True,
-                      help='squelch all informational output')
-    parser.add_option('-p', '--local-port', action='store', type='int', dest='port',
-                      default=DEFAULT_PORT,
-                      help='local port to forward (default: %d)' % DEFAULT_PORT)
-    parser.add_option('-u', '--user', action='store', type='string', dest='user',
-                      default=getpass.getuser(),
-                      help='username for SSH authentication (default: %s)' % getpass.getuser())
-    parser.add_option('-K', '--key', action='store', type='string', dest='keyfile',
-                      default=None,
-                      help='private key file to use for SSH authentication')
-    parser.add_option('', '--no-key', action='store_false', dest='look_for_keys', default=True,
-                      help='don\'t look for or use a private key file')
-    parser.add_option('-P', '--password', action='store_true', dest='readpass', default=False,
-                      help='read password (for key or password auth) from stdin')
-    parser.add_option('-r', '--remote', action='store', type='string', dest='remote', default=None, metavar='host:port',
-                      help='remote host and port to forward to')
-    options, args = parser.parse_args()
-
-    if len(args) != 1:
-        parser.error('Incorrect number of arguments.')
-    if options.remote is None:
-        parser.error('Remote address required (-r).')
-    
-    g_verbose = options.verbose
-    server_host, server_port = get_host_port(args[0], SSH_PORT)
-    remote_host, remote_port = get_host_port(options.remote, SSH_PORT)
-    return options, (server_host, server_port), (remote_host, remote_port)
-
-
-def main():
-    options, server, remote = parse_options()
-    
+def get_tunnel(targethost, targetport, sshclient):
 
     # reasons to use satori's modified version of paramiko.SSHClient
     #   1) hackishly tries to respond to password prompts
     #   2) responds to demands for a TTY by reconnecting with a pseudo tty
     #   3) prioritizes authentication mechanisms
-    
+
     # TODO: change this to satori.ssh and give some thought to bash.RemoteClient
-
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy())
-
-    verbose('Connecting to ssh host %s:%d ...' % (server[0], server[1]))
-    try:
-        client.connect(server[0], server[1], username=options.user, key_filename=options.keyfile,
-                       look_for_keys=options.look_for_keys, password=password)
-    except Exception as e:
-        print('*** Failed to connect to %s:%d: %r' % (server[0], server[1], e))
-        sys.exit(1)
-
-    verbose('Now forwarding port %d to %s:%d ...' % (options.port, remote[0], remote[1]))
-
-    try:
-        forward_tunnel(options.port, remote[0], remote[1], client.get_transport())
-    except KeyboardInterrupt:
-        print('C-c: Port forwarding stopped.')
-        sys.exit(0)
+    return Tunnel(targethost, targetport, sshclient)
 
 
-if __name__ == '__main__':
-    main()
+def get_sshclient(bastionhost, pkeystring, username="lnx-waldo"):
+
+    client = ssh.connect(bastionhost, username=username,
+                         private_key=pkeystring,
+                         options={'StrictHostKeyChecking': False})
+
+    return client
+
